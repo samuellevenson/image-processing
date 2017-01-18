c85c575e47641f60cddbf25c481cb2a45adda22c
samuellevenson <samuel.levenson11@gmail.com>
2017-01-18 05:48:22 -0500

    Add files via upload

--- GaussianBlur.java
+++ GaussianBlur.java
@@ -26,7 +26,8 @@ import javax.swing.event.ChangeListener;
  */
 public class GaussianBlur {
   public static Picture orig;
-  public static Picture blurred;
+  public static Picture lowpass;
+  public static Picture highpass;
   public static double sd = 1;
   public static int r = 1;
   private static JFrame frame;
@@ -36,7 +37,8 @@ public class GaussianBlur {
   public static void main(String[] args) {
     File in = new File(args[0]);
     orig = new Picture(in);
-    blurred = padEdges(orig);
+    lowpass = orig;
+    highpass = orig;
     makeSliders();
     updateBlur();
   }
@@ -45,10 +47,11 @@ public class GaussianBlur {
     frame = new JFrame("Sliders");
     frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     //standard deviation slider
-    sdSlider = new JSlider(1,10,(int)sd);
+    sdSlider = new JSlider(1,10,(int)(sd));
     sdSlider.setPaintTicks(true);
     sdSlider.setPaintLabels(true);
     sdSlider.setMajorTickSpacing(1);
+    sdSlider.setSnapToTicks(true);
     sdSlider.addChangeListener(new ChangeListener() {
       public void stateChanged(ChangeEvent e) {
         JSlider source = (JSlider)e.getSource();
@@ -70,6 +73,7 @@ public class GaussianBlur {
     rSlider.setPaintTicks(true);
     rSlider.setPaintLabels(true);
     rSlider.setMajorTickSpacing(1);
+    rSlider.setSnapToTicks(true);
     rSlider.addChangeListener(new ChangeListener() {
       public void stateChanged(ChangeEvent e) {
         JSlider source = (JSlider)e.getSource();
@@ -94,17 +98,23 @@ public class GaussianBlur {
    * blur image based on updated values of sd and r and show new image
    */
   public static void updateBlur() {
-    double[][] kernel = makeKernel();
-    Picture res = new Picture(orig.width(),orig.height());
-    //iterates through each pixel in image exluding a border based on r
-    for(int x = r; x < orig.width() - r; x++) {
-      for(int y = r; y < orig.height() - r; y++) {
-        //apply kernel to each pixel to get value for blurry image
-        res.set(x,y,applyKernel(kernel, orig, x, y));
+    lowpass = orig;
+    highpass = orig;
+    double[][] lp = lowpassKernel();
+    double[][] hp = highpassKernel();
+    for(int x = 0; x < orig.width(); x++) {
+      for(int y = 0; y < orig.height(); y++) {
+        //apply kernel to each pixel to get value for new image
+        lowpass.set(x,y,applyKernel(lp, x, y));
+        highpass.set(x,y,applyKernel(hp, x, y));
       }
     }
-    res = removePadding(res);
-    res.show();
+    System.out.println("sd: " + sd);
+    printKernel(lp);
+    System.out.println();
+    printKernel(hp);
+    lowpass.show();
+    highpass.show();
   }
   /**
    * 2D gaussian function (turns out the 1D equivalent expresses the normal distribution from statistics class!)
@@ -112,9 +122,87 @@ public class GaussianBlur {
   public static double g(int x, int y) {
     return ((1/(2*Math.PI*sd*sd)) * Math.exp(-1*(x*x + y*y)/(2*sd*sd))); 
   }
+  
+  /**
+   * r sized matrix aprox. of gaussian function where x and y are distance from center of matrix
+   * dimensions of matrix (and therefore r) must be odd
+   */
+  public static double[][] lowpassKernel() {
+    double[][] kernel = new double[(2*r)+1][(2*r)+1];
+    int xmid = r;
+    int ymid = r; //square
+    double sum = 0;
+    for(int x = 0; x < kernel.length; x++) {
+      for(int y = 0; y < kernel[x].length; y++) {
+        kernel[x][y] = g(x - xmid, y - ymid);
+        sum += kernel[x][y];
+      }
+    }
+    double sum2 = 0;;
+    for(int x = 0; x < kernel.length; x++) {
+      for(int y = 0; y < kernel[x].length; y++) {
+        kernel[x][y] = (kernel[x][y])/sum; //sum of elements in kernel should equal 1
+        sum2 += kernel[x][y];
+      }
+    }
+    System.out.println("lowpass:");
+    System.out.println("sum1: " + sum);
+    System.out.println("sum2: " + sum2);
+    return kernel;
+  }
+  /**
+   * high pass equivalent of above method
+   */
+  public static double[][] highpassKernel() {
+    double[][] kernel = new double[(2*r)+1][(2*r)+1];
+    int xmid = r;
+    int ymid = r; //square
+    double sum = 0;
+    double height = 1.0/(Math.sqrt(2*Math.PI) * sd); //height of normal curve
+    for(int x = 0; x < kernel.length; x++) {
+      for(int y = 0; y < kernel[x].length; y++) {
+        kernel[x][y] = height - g(x - xmid, y - ymid);
+        sum += kernel[x][y];
+      }
+    }
+    double sum2 = 0;
+    for(int x = 0; x < kernel.length; x++) {
+      for(int y = 0; y < kernel[x].length; y++) {
+        kernel[x][y] = ((kernel[x][y])/sum);
+        sum2 += kernel[x][y];
+      }
+    }
+    System.out.println("highpass:");
+    System.out.println("sum1: " + sum);
+    System.out.println("sum2: " + sum2);
+    return kernel;
+  }
+  /**
+   * returns color of pixel once kernel has been applied
+   */
+  public static Color applyKernel(double[][] kernel, int x, int y) {
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+    for(int dx = -r; dx <= r; dx++) {
+      for(int dy = -r; dy <= r; dy++) {
+        int x1 = x + dx;
+        int y1 = y + dy;
+        //check if pixel is valid
+        if(x1 < 0 || y1 < 0 || x1 >= orig.width() || y1 >= orig.height()) {
+          continue;
+        }
+        red += (int)((orig.get(x1,y1).getRed()) * (kernel[dx + r][dy + r]));
+        green += (int)((orig.get(x1,y1).getGreen()) * (kernel[dx + r][dy + r]));
+        blue += (int)((orig.get(x1,y1).getBlue()) * (kernel[dx + r][dy + r]));
+      }
+    }
+    return new Color(red,green,blue);
+  }
   /**
    * increases image size by r on all sides and pads image so that the padding contains the same
    * value as the closest pixel in the original image
+   * not in use at the moment
    */
   public static Picture padEdges(Picture orig) {
     Picture res = new Picture(orig.width() + 2*r, orig.height() + 2*r);
@@ -172,49 +260,11 @@ public class GaussianBlur {
         res.set(x,y,orig.get(orig.width()-1,y-r));
       }
     }
-    
     return res;
   }
   /**
-   * r sized matrix aprox. of gaussian function where x and y are distance from center of matrix
-   * dimensions of matrix (and therefore r) must be odd
-   */
-  public static double[][] makeKernel() {
-    double[][] kernel = new double[(2*r)+1][(2*r)+1];
-    int xmid = r;
-    int ymid = r; //square
-    double sum = 0;
-    for(int x = 0; x < kernel.length; x++) {
-      for(int y = 0; y < kernel[x].length; y++) {
-        kernel[x][y] = g(x - xmid, y - ymid);
-        sum += kernel[x][y];
-      }
-    }
-    for(int x = 0; x < kernel.length; x++) {
-      for(int y = 0; y < kernel[x].length; y++) {
-        kernel[x][y] = kernel[x][y]/sum;
-      }
-    }
-    return kernel;
-  }
-  /**
-   * returns color of pixel once kernel has been applied
-   */
-  public static Color applyKernel(double[][] kernel, Picture orig, int x, int y) {
-    int red = 0;
-    int green = 0;
-    int blue = 0;
-    for(int dx = -r; dx <= r; dx++) {
-      for(int dy = -r; dy <= r; dy++) {
-        red += ((orig.get(x + dx,y + dy).getRed()) * (kernel[dx + r][dy + r]));
-        green += ((orig.get(x + dx,y + dy).getGreen()) * (kernel[dx + r][dy + r]));
-        blue += ((orig.get(x + dx,y + dy).getBlue()) * (kernel[dx + r][dy + r]));
-      }
-    }
-    return new Color(red,green,blue);
-  }
-  /**
    * padding has served its purpose and shows up as black pixels in blurred image, remove it
+   * not in use right now
    */
   public static Picture removePadding(Picture orig) {
     Picture res = new Picture(orig.width() - 2*r, orig.height() - 2*r);
@@ -224,5 +274,13 @@ public class GaussianBlur {
       }
     }
     return res;
+  }
+  public static void printKernel(double[][] k) {
+    for(double[] row: k) {
+      for(double col: row) {
+        System.out.print(col + " ");
+      }
+      System.out.println();
+    }
   }
 }

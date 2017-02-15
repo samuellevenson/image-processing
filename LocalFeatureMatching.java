6fa2c3fb26a52efca5336a9b62e604a3b8053218
samuellevenson <samuel.levenson11@gmail.com>
2017-02-15 04:21:33 -0500

    Add files via upload

--- LocalFeatureMatching.java
+++ LocalFeatureMatching.java
@@ -1,27 +1,29 @@
 import java.awt.Color;
 import java.io.File;
+import java.util.ArrayList;
 /**
  * resources: http://cs.brown.edu/courses/cs143/2013/proj2/ (project page)
  *            http://mccormickml.com/2013/02/26/image-derivative/ (image derivatives)
  *            http://fourier.eng.hmc.edu/e161/lectures/gradient/node8.html (laplacian operator + gaussian things)
- *            https://en.wikipedia.org/wiki/Kronecker_product (how to do outer product of matrices)
+ *            https://www.youtube.com/watch?v=vkWdzWeRfC4
  * 
  * 2/8/17
  * 
  * started interest point detection part of program
  */
 public class LocalFeatureMatching {
+  
+  public static Picture i; //original image
+  public static Picture ix; //x derivative
+  public static Picture iy; //y derivative
+  public static int r = 1; //windowing radius
+  public static double threshold = 0; //minimun value for a pixel to be a corner
+  
   /**
-   * 2D gaussian function (turns out the 1D equivalent expresses the normal distribution from statistics class!)
+   * 2D gaussian function
    */
   public static double gaussian(int x, int y, double sd) {
     return ((1/(2*Math.PI*sd*sd)) * Math.exp(-1*(x*x + y*y)/(2*sd*sd))); 
-  }
-  /**
-   * laplacian of gaussian combines laplacian operator with gaussian function
-   */
-  public static double laplacianOfGaussian(int x, int y, double sd) {
-    return Math.exp(-1*(x*x+y*y)/2*sd*sd) * (x*x + y*y - 2*sd*sd)/(sd*sd*sd*sd);
   }
   /**
    * apples a given kernel to a given image
@@ -59,7 +61,7 @@ public class LocalFeatureMatching {
     Picture result = new Picture(img.width(),img.height());
     for(int y = 0; y < img.height(); y++) {
       for(int x = 1; x < img.width(); x++) {
-        int dx = img.get(x,y).getRed() - img.get(x-1,y).getRed();
+        int dx = (int)(rgbAvg(img.get(x,y)) - rgbAvg(img.get(x-1,y)));
         dx = (dx+255)/2; //scale from range of -255,255 to range of 0,255
         result.set(x,y,new Color(dx,dx,dx));
       }
@@ -73,7 +75,7 @@ public class LocalFeatureMatching {
     Picture result = new Picture(img.width(),img.height());
     for(int x = 0; x < img.width(); x++) {
       for(int y = 1; y < img.height(); y++) {
-        int dy = img.get(x,y).getRed() - img.get(x,y-1).getRed();
+        int dy = (int)(rgbAvg(img.get(x,y)) - rgbAvg(img.get(x,y-1)));
         dy = (dy+255)/2; //scale from range of -255,255 to range of 0,255
         result.set(x,y,new Color(dy,dy,dy));
       }
@@ -83,12 +85,12 @@ public class LocalFeatureMatching {
   /**
    * gaussian blur of image
    */
-  public static Picture gBlur(Picture img, int r, double sd) {
+  public static Picture gBlur(Picture img, double sd) {
     //make gaussian kernel
     Picture result = new Picture(img);
     double[][] g = new double[(2*r)+1][(2*r)+1];
     int xmid = r;
-    int ymid = r; //square
+    int ymid = r;
     double sum = 0;
     for(int x = 0; x < g.length; x++) {
       for(int y = 0; y < g[x].length; y++) {
@@ -107,65 +109,107 @@ public class LocalFeatureMatching {
     return result;
   }
   /**
-   * applies laplacian operator + gaussian blur
+   * multiplies two 2D matrices together
+   * will probably not work if dimensions don't line up like they need to when you multiply matrices
    */
-  public static Picture lapLogBlur(Picture img, int r, double sd) {
-    //make laplacian of gaussian kernel (LoG)
-    Picture result = new Picture(img);
-    double[][] lapLog = new double[(2*r)+1][(2*r)+1];
-    int xmid = r;
-    int ymid = r; //square
-    double sum = 0;
-    for(int x = 0; x < lapLog.length; x++) {
-      for(int y = 0; y < lapLog[x].length; y++) {
-        lapLog[x][y] = laplacianOfGaussian(x - xmid, y - ymid, sd);
-        sum += lapLog[x][y];
-      }
-    }
-    //make elements of kernel sum to 1
-    for(int x = 0; x < lapLog.length; x++) {
-      for(int y = 0; y < lapLog[x].length; y++) {
-        lapLog[x][y] = (lapLog[x][y])/sum;
-      }
-    }
-    //apply kernel
-    result = applyKernel(img,lapLog);
-    return result;
-  }
-  /**
-   * unfinished attempt to find outer product of two matrices
-   */
-  public static Picture outerProduct(Picture p1, Picture p2) {
-    Picture outer = new Picture(p1.width()*p2.width(), p1.height()*p2.height());
-    for(int x1 = 0; x1 < p1.width(); x1++) {
-      for(int y1 = 0; y1 < p1.height(); y1++) {
-        int red = p1.get(x1,y1).getRed();
-        int green = p1.get(x1,y1).getGreen();
-        int blue = p1.get(x1,y1).getBlue();
-        for(int x2 = 0; x2 < p2.width(); x2++) {
-          for(int y2 = 0; y2 < p2.height(); y2++) {
-            
-          }
+  public static double[][] matrixMultiply(double[][] m1, double[][] m2) {
+    double[][] mresult = new double[m1.length][m2[0].length];
+    for(int m1Row = 0; m1Row < m1.length; m1Row++) {
+      for(int m2Col = 0; m2Col < m2[0].length; m2Col++) {
+        for(int m1Col = 0; m1Col < m1[0].length; m1Col++) {
+          mresult[m1Row][m2Col] += m1[m1Row][m1Col] * m2[m1Col][m2Col];
         }
       }
     }
-    return null;
+    return mresult;
+  }
+  /**
+   * for a given point (x,y) on the image finds a matrix
+   *  | ix*ix ix*iy |
+   *  | ix*iy iy*iy | 
+   * where ix is the sum of the pixel values in the x image derivative in a radius r from point (x,y)
+   */
+  public static double[][] autoCorrelation(int x0, int y0) {
+    double ixSum = 0;
+    double iySum = 0;
+    for(int x = x0 - r; x < x0 + 2*r; x++) {
+      for(int y = y0 - r; y < y0 + 2*r; y++) {
+        ixSum += rgbAvg(ix.get(x,y));
+        iySum += rgbAvg(iy.get(x,y));
+      }
+    }
+    return new double[][] {{ixSum * ixSum, ixSum * iySum},{ixSum * iySum, iySum * iySum}};
+  }
+  /**
+   * finds the average of the red, green, and blue in a color
+   */
+  public static double rgbAvg(Color c) {
+    return (c.getRed() + c.getGreen() + c.getBlue())/3.0;
+  }
+  /**
+   * finds the smaller of two eigenvalues of a 2x2 matrix
+   */
+  public static double eigenVal(double[][] m) {
+    double a = m[0][0];
+    double b = m[0][1];
+    double c = m[1][0];
+    double d = m[1][1];
+    double l1 = 0.5 * (a-d) + Math.sqrt(((a-d)*(a-d)) - 4*((a*d)-(b*c)));
+    double l2 = 0.5 * (a-d) - Math.sqrt(((a-d)*(a-d)) - 4*((a*d)-(b*c)));
+    if(l1 < l2) {
+      return l1;
+    }
+    return l2;
+  }
+  /**
+   * 
+   */
+  public static ArrayList<int[]> findCorners() {
+    ArrayList<int[]> corners = new ArrayList<int[]>();
+    //goes through image except for r sized border
+    for(int x = r; x < i.width() - r; x++) {
+      for(int y = r; y < i.height() - r; y++) {
+        double[][] m = autoCorrelation(x,y); //finds autocorrelation matrix
+        double l = eigenVal(m); //smaller eigenvalue of matrix m
+        if(l > threshold) {
+          corners.add(new int[] {x,y});
+        }
+      }
+    }
+    return corners;
+  }
+  /**
+   * 
+   */
+  public static void drawCorners() {
+    ArrayList<int[]> corners = findCorners();
+    Picture withCorners = new Picture(i);
+    for(int i = 0; i < corners.size(); i++) {
+      int x0 = corners.get(i)[0];
+      int y0 = corners.get(i)[1];
+      for(int x = x0 - 1; x < x0 + 2; x++) {
+        for(int y = y0 - 1; y < y0 + 2; y++) {
+          withCorners.set(x,y, new Color(255,0,0)); //sets 3x3 area centered on corner red
+        }
+      }
+    }
+    withCorners.show();
   }
   public static void main(String[] args) {
-    Picture img = new Picture(new File("/Users/sammy/Documents/compsi/compsci12th/pictures/bicycle.bmp"));
-    if(args.length > 0) {
-      img = new Picture(new File(args[0]));
+    if(args.length == 0) {
+      i = new Picture(new File("/Users/sammy/Documents/compsi/compsci12th/pictures/bicycle.bmp"));
     }
-    img.show();
-    img.setTitle("original");
-    Picture imgx = xDeriv(gBlur(img,1,1)); //image derivative in the x direction
-    imgx.show();
-    imgx.setTitle("x derivative");
-    Picture imgy = yDeriv(gBlur(img,1,1)); //image derivative in the y direction
-    imgy.show();
-    imgy.setTitle("y derivative");
-    Picture laplacianOfGaussian = lapLogBlur(img,1,1);
-    laplacianOfGaussian.show();
-    laplacianOfGaussian.setTitle("laplacian of gaussian");
+    else {
+      i = new Picture(new File(args[0])); 
+    }
+    i.show();
+    i.setTitle("original");
+    ix = gBlur(xDeriv(i),1); //image derivative in the x direction
+    ix.show();
+    ix.setTitle("x derivative");
+    iy = gBlur(yDeriv(i),1); //image derivative in the y direction
+    iy.show();
+    iy.setTitle("y derivative");
+    drawCorners();
   }
 }
